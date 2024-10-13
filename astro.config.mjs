8855f13c86a379a18b69a9fa349b373760123d10
Yannis MOKHTARI <ymokhtari@bigben-connected.com>
2024-10-13 18:30:14 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
-
 import tailwind from "@astrojs/tailwind";
+import dotenv from 'dotenv';
 
+// Charger le bon fichier d'environnement en fonction du mode
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });  // Production
+  console.log("En mode production, base URL :", process.env.PUBLIC_BASE_URL);
+} else {
+  dotenv.config({ path: '.env' });  // Développement
+  console.log("En mode développement, base URL :", process.env.PUBLIC_BASE_URL);
+}
 
 // // https://astro.build/config
 export default defineConfig({
     integrations: [icon(), /*tailwind()*/],
-     output: 'static',
+    output: 'static',
     site: 'https://Mokhtariyannis.portfolio.github.io',
-    base: import.meta.env.PUBLIC_BASE_URL || '/',
+    base: process.env.PUBLIC_BASE_URL || '/',  // Utilisation de PUBLIC_BASE_URL avec fallback
 });
